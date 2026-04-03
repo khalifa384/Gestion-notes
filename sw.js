e932d46ef9cccf5c6e6c8a46effafe3e0190b8a7
khalifa384 <65577826+khalifa384@users.noreply.github.com>
2026-04-03 15:00:32 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'notes-app-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './gestion_notes.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache).catch(() => {
+        console.log('Certains fichiers n\'ont pas pu être cachés');
+      });
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).then(response => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    }).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
